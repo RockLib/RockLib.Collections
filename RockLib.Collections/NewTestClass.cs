f4d2c2651168af2e2464ee2d4db860d0197d3c79
Eric Slutz <ericslutz@rocketmortgage.com>
2022-10-24 12:06:17 -0400

    added new empty class

---
+++ RockLib.Collections/NewTestClass.cs
@@ -0,0 +1,10 @@
+namespace RockLib.Collections
+{
+    /// <summary>
+    /// Empty class created for testing purposes to make a PR that has changes from original.
+    /// </summary>
+    public class NewTestClass
+    {
+
+    }
+}
